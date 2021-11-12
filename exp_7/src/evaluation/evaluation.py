dd9322f47b77edf219ffc753d3b673a7d0fd9142
haraso <haraso@jxpress.net>
2021-11-12 16:58:53 +0900

    bug fix

--- exp_7/src/evaluation/evaluation.py
+++ exp_7/src/evaluation/evaluation.py
@@ -8,33 +8,30 @@ from evaluation.utils import rank_order
 
 def top_1_auc(oof: Dict[str, np.array]) -> Tuple[str, float]:
     rank_out = rank_order(oof["rank_out"])
-    rank_target = rank_order(oof["rank_target"])
-    print(rank_out)
-    top1_labels_for_auc = (rank_target == 0).astype(int)
-    print(rank_target)
-    print(top1_labels_for_auc)
+    rank_target = oof["rank_target"].astype(int)
+    top1_labels_for_auc = (rank_target == 1).astype(int)
     top_1_auc_score = roc_auc_score(top1_labels_for_auc, rank_out)
     return "top_1_auc", top_1_auc_score
 
 
 def top3_ndcg_score(oof: Dict[str, np.array]) -> Tuple[str, float]:
     rank_out = rank_order(oof["rank_out"])
-    rank_target = rank_order(oof["rank_target"])
+    rank_target = oof["rank_target"].astype(int)
     top_3_ndcg_score = ndcg_score(1 / rank_target, 1 / rank_out, k=3)
     return "top_3_ndcg", top_3_ndcg_score
 
 
 def top_1_auc_from_time(oof: Dict[str, np.array]) -> Tuple[str, float]:
     rank_out = rank_order(oof["time_out"])
-    rank_target = rank_order(oof["rank_target"])
-    top1_labels_for_auc = (rank_target == 0).astype(int)
+    rank_target = oof["rank_target"].astype(int)
+    top1_labels_for_auc = (rank_target == 1).astype(int)
     top_1_auc_score = roc_auc_score(top1_labels_for_auc, rank_out)
     return "top_1_auc_from_time", top_1_auc_score
 
 
 def top3_ndcg_score_from_time(oof: Dict[str, np.array]) -> Tuple[str, float]:
     rank_out = rank_order(oof["time_out"])
-    rank_target = rank_order(oof["rank_target"])
+    rank_target = oof["rank_target"].astype(int)
     top_3_ndcg_score = ndcg_score(1 / rank_target, 1 / rank_out, k=3)
     return "top_3_ndcg_from_time", top_3_ndcg_score
 
